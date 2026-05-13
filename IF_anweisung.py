453df82041c27f509a0a1349fe7e2d4addf92a2d
narodin <m.jessenitschnig@gmx.at>
2026-05-13 13:08:04 +0200

    Unnötiges gelöscht und If-Anweisung und Vergleichsoperatoren hinzugefügt.

---
+++ IF_anweisung.py
@@ -0,0 +1,8 @@
+age = int(input("Geben Sie Ihr Alter ein: "))
+
+if age < 18:
+    print("Achtung! Sie sind minderjährig.")
+elif age >= 18:
+    print("Sie sind volljährig.")
+
+print("Ende des Programms.")
---
+++ vergleichsoperator.py
@@ -0,0 +1,7 @@
+print(1 > 5) # (<) kleiner als, (>) größer als, (<=) kleiner oder gleich, (>=) größer oder gleich, (==) gleich, (!=) ungleich
+
+print(5 >= 5) # True, da 5 größer oder gleich 5 ist
+
+print(4 == 5) # (==) gleich Operator, (!=) ungleich Operator
+
+variable = True # Variable mit booleschem Wert, kann entweder True oder False sein
